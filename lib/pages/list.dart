fd04c314f6244f22b41e0427ad281207cd28366d
RedmiAndrian <63767615+RedmiAndrian@users.noreply.github.com>
2020-05-27 23:57:31 +0800

    Just Updated list.dart
    Just making this code to become more short and tidy.

--- lib/pages/list.dart
+++ lib/pages/list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:planetinfo/classes/planets.dart';
 
 class PlanetInfo extends StatefulWidget {
   @override
@@ -7,6 +8,33 @@ class PlanetInfo extends StatefulWidget {
 
 class _PlanetInfoState extends State<PlanetInfo> {
 
+  List<Planets> planets= [
+    Planets(name: 'Mercury', image:'assets/mercury.jpg', routes:'/mercury'),
+    Planets(name: 'Venus', image:'assets/venus.jpg', routes: '/venus'),
+    Planets(name: 'Earth', image:'assets/earth.jpg', routes: '/earth'),
+    Planets(name: 'Mars', image:'assets/mars.jpg', routes:'/mars'),
+    Planets(name:'Jupiter', image:'assets/jupiter.jpeg', routes:'/jupiter'),
+    Planets(name: 'Saturn', image:'assets/saturn.png', routes:'/saturn'),
+    Planets(name: 'Uranus',image: 'assets/uranus.jpg',routes: '/uranus',),
+    Planets(name: 'Neptune',image: 'assets/neptune.jpg',routes: '/neptune',),
+  ];
+
+  Widget planetTemplate(planet){
+    return GestureDetector(
+              onTap: (){
+                Navigator.pushNamed(context, planet.routes);
+              },
+              child: Card(
+                color:  Colors.black,
+                child: ListTile(
+                  leading: Image.asset(planet.image),
+                  title: Text(planet.name, textAlign: TextAlign.center,),
+              ),
+              ),
+            );
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,114 +42,9 @@ class _PlanetInfoState extends State<PlanetInfo> {
         title: Text('Info of the Planet', style: TextStyle(fontSize:20,),),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: <Widget>[
-            GestureDetector(
-              onTap: () {
-                Navigator.pushNamed( context, '/mercury');
-              },
-              child: Card(
-                color:  Colors.black,
-                child: ListTile(
-        leading: Image.asset('assets/mercury.jpg'),
-        title: Text(
-          'Mercury',
-          textAlign: TextAlign.center,
-          ),
-      ),
-
-              ),
-            ),
-            GestureDetector(
-              onTap: (){
-                Navigator.pushNamed(context, '/venus');
-              },
-              child: Card(
-                color:  Colors.black,
-                child: ListTile(
-                  leading: Image.asset('assets/venus.jpg'),
-                  title: Text('Venus', textAlign: TextAlign.center,),
-              ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, '/earth');
-              },
-              child: Card(
-                color:  Colors.black,
-                child: ListTile(
-                  leading: Image.asset('assets/earth.jpg'),
-                  title: Text('Earth', textAlign: TextAlign.center,),
-              ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, '/mars');
-              },
-              child: Card(
-                color:  Colors.black,
-                child: ListTile(
-                  leading: Image.asset('assets/mars.jpg'),
-                  title: Text('Mars', textAlign: TextAlign.center,),
-              ),
-              ),
-            ),
-            GestureDetector(
-              onTap: (){
-                Navigator.pushNamed(context, '/jupiter');
-              },
-              child: Card(
-                color:  Colors.black,
-                child: ListTile(
-                  leading: Image.asset('assets/jupiter.jpeg'),
-                  title: Text('Jupiter', textAlign: TextAlign.center,),
-              ),
-              ),
-            ),
-            GestureDetector(
-              onTap:(){
-                Navigator.pushNamed(context, '/saturn');
-              },
-              child: Card(
-                color:  Colors.black,
-                child: ListTile(
-                  leading: Image.asset('assets/saturn.png'),
-                  title: Text('Saturn', textAlign: TextAlign.center,),
-              ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, '/uranus');
-              },
-              child: Card(
-                color:  Colors.black,
-                child: ListTile(
-                  leading: Image.asset('assets/uranus.jpg'),
-                  title: Text('Uranus', textAlign: TextAlign.center,),
-              ),
-              ),
-            ),
-            GestureDetector(
-              onTap: (){
-                Navigator.pushNamed(context, '/neptune');
-              },
-              child: Card(
-                color:  Colors.black,
-                child: ListTile(
-                  leading: Image.asset('assets/neptune.jpg'),
-                  title: Text('Neptune' ,textAlign: TextAlign.center,),
-              ),
-              ),
-            ),
-            
-            ],
-        ),
-      ),
+      body:  Column(
+        children: planets.map((planet) => planetTemplate(planet)).toList(),
+      )
     );
   }
 }
